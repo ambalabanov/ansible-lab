4e7bc625f1e790d38ad66b353c13668d9ed37692
Andrey Balabanov <a.balabanov@icloud.com>
2021-11-15 18:19:09 +0500

    rename playbooks

--- playbooks/10-useradd.sh
+++ playbooks/10-useradd.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ansible all -m user -a "name=vagrant state=present"
+ansible all -m user -a "name=vagrant state=present generate_ssh_key=yes"
 ansible all -m file -a "path=/home/vagrant/.ssh state=directory mode=700"
 ansible all -m copy -a 'content="vagrant ALL=(ALL) NOPASSWD:ALL\n" dest=/etc/sudoers.d/vagrant'
 ansible all -m authorized_key -a "user=vagrant key={{ lookup('file', '/home/vagrant/.ssh/id_rsa.pub') }} state=present"
