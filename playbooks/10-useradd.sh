4825bfc1765b632f244aac9d1a1b889827f7eef5
Andrey Balabanov <a.balabanov@icloud.com>
2021-11-20 17:02:12 +0500

    adhoc and playbook are aligned

--- playbooks/10-useradd.sh
+++ playbooks/10-useradd.sh
@@ -2,6 +2,6 @@
 
 ansible all -m user -a "name=vagrant state=present generate_ssh_key=yes"
 ansible all -m file -a "path=/home/vagrant/.ssh state=directory mode=700"
-ansible all -m copy -a 'content="vagrant ALL=(ALL) NOPASSWD:ALL\n" dest=/etc/sudoers.d/vagrant'
+ansible all -m copy -a 'content="vagrant ALL=(ALL) NOPASSWD: ALL\n" dest=/etc/sudoers.d/vagrant'
 ansible all -m authorized_key -a "user=vagrant key={{ lookup('file', '/home/vagrant/.ssh/id_rsa.pub') }} state=present"
-
+ansible all -m ping -u vagrant
