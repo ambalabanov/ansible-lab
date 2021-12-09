8ed011f0c41631e2d4d56c82c413da28c51a8035
Andrey Balabanov <a.balabanov@icloud.com>
2021-12-09 15:48:09 +0500

    idempotent adhoc

--- playbooks/10-useradd.sh
+++ playbooks/10-useradd.sh
@@ -1,7 +1,13 @@
-#! /bin/bash
-
-ansible all -m user -a "name=vagrant state=present generate_ssh_key=yes"
-ansible all -m file -a "path=/home/vagrant/.ssh state=directory mode=700"
-ansible all -m copy -a 'content="vagrant ALL=(ALL) NOPASSWD: ALL\n" dest=/etc/sudoers.d/vagrant'
-ansible all -m authorized_key -a "user=vagrant key={{ lookup('file', '/home/vagrant/.ssh/id_rsa.pub') }} state=present"
-ansible all -m ping -u vagrant
+#!/usr/bin/env bash
+# run under vagrant user
+sudo ansible all -b -u root -m user -a "name=vagrant password={{ 'vagrant' | password_hash('sha512','salt') }} generate_ssh_key=yes"
+sudo ansible all -b -u root -m copy -a "content='vagrant ALL=(ALL) NOPASSWD: ALL' dest=/etc/sudoers.d/vagrant"
+HOSTS=$(ansible all --list-hosts |tail -n 5)
+rm /home/vagrant/.ssh/known_hosts
+for HOST in $HOSTS
+do
+    ssh-keyscan $HOST >> ~/.ssh/known_hosts 2>/dev/null
+done
+ansible all -m file -a "path=/home/vagrant/.ssh state=directory mode=0700" -e "ansible_user=vagrant ansible_password=vagrant"
+ansible all -m authorized_key -a "user=vagrant key={{ lookup('file', '/home/vagrant/.ssh/id_rsa.pub') }} state=present" -e "ansible_user=vagrant ansible_password=vagrant"
+ansible all -m ping
